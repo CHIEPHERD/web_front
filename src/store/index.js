3d382f5b6f2a3c94af1efd3308f9789a40ac9aea
yohan fairfort <yohan.fairfort@netapsys.fr>
2017-07-17 00:51:01 +0200

    Store of project in progress

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueX from 'vuex'
 
 import auth from './auth'
+import project from './projects'
 
 Vue.use(VueX)
 
 export default new VueX.Store({
   modules: {
-    auth: auth
+    auth: auth,
+    project: project
   }
 })
